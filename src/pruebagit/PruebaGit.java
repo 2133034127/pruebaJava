466d04ee043216260cb4dad8875d63edf4cf402e
CarlosJerack <al2133034127@azc.uam.mx>
2021-05-15 13:09:40 -0500

    Prueba 1 de commit

---
+++ src/pruebagit/PruebaGit.java
@@ -0,0 +1,15 @@
+package pruebagit;
+
+/**
+ * @author Hernandez Zarazua Carlos Jerack
+ */
+public class PruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Usando git");
+    }
+    
+}
